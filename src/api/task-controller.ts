1ae2f50c21f6ed73821724916d3f8f8183e27cdb
VipulaNeranjana <vipulana17@gmail.com>
2023-06-21 19:27:18 +0530

    implement delete method

--- src/api/task-controller.ts
+++ src/api/task-controller.ts
@@ -46,3 +46,9 @@ router.post("/",async (req,res)=>{
     res.status(201).json(task);
 });
 
+/* Delete an existing task */
+router.delete("/:taskId", async (req,res)=>{
+    const result = await pool.query("DELETE FROM task WHERE id=?",[req.params.taskId]);
+    res.sendStatus(result.affectedRows ? 204:404);
+});
+
